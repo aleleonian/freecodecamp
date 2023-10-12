e3021bbb7beb439e37a125af0f0d945e375fbf24
Alejandro Leonian <alejandro.leonian@humansecurity.com>
2023-10-12 11:01:22 -0500

    improved solution to 14

--- Javascript_DSA/Intermediate_Algorithm_Scripting/14-smallestCommon.js
+++ Javascript_DSA/Intermediate_Algorithm_Scripting/14-smallestCommon.js
@@ -7,27 +7,69 @@ function smallestCommons(arr) {
     for (let i = 1; ; i++) {
         smallestCommon = second * i;
 
-        let foundIt = false;
-
         if (smallestCommon % first == 0 && smallestCommon % second == 0) {
-            for (let x = first + 1; x < second; x++) {
-                if (smallestCommon % x == 0) {
-                    foundIt = true;
-                }
-                else {
-                    foundIt = false;
-                    break;
-                }
+            if (isItDivisableByTheRange(first, second, smallestCommon)) {
+                break;
             }
-        }
-        if (foundIt) {
-            console.log("Found them!->smallestCommon =" + smallestCommon);
-            smallestCommon = smallestCommon;
-            break;
         }
     }
 
     return smallestCommon;
 }
 
+function isItDivisableByTheRange(startingNumber, endingNumber, candidate) {
+    for (let i = startingNumber + 1; i < endingNumber; i++) {
+        if (candidate % i != 0) {
+            return false;
+        }
+    }
+    return true;
+}
 smallestCommons([1, 5]);
+
+/*
+
+ChatGPT's version:
+
+The provided function `smallestCommons` is a valid implementation to find the smallest common multiple (LCM) of a range of numbers. However, it can be simplified and improved for readability. Here's a more concise and efficient version:
+
+```javascript
+function smallestCommons(arr) {
+    // Sort the input array in ascending order
+    arr.sort((a, b) => a - b);
+
+    // Initialize the LCM as the largest number in the range
+    let lcm = arr[1];
+
+    // Calculate LCM for the range of numbers
+    for (let i = arr[0]; i < arr[1]; i++) {
+        lcm = (lcm * i) / gcd(lcm, i);
+    }
+
+    return lcm;
+}
+
+// Helper function to calculate the greatest common divisor (GCD)
+function gcd(a, b) {
+    if (b === 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+// Example usage:
+const result = smallestCommons([1, 5]);
+console.log("Smallest Common Multiple:", result);
+```
+
+In this revised code:
+
+1. We sort the input array to ensure that the numbers are in ascending order.
+
+2. We initialize the LCM as the second number (the largest) in the range. This is a starting point for finding the LCM.
+
+3. We iterate through the range of numbers and use the GCD function to calculate the LCM. This is a more efficient approach as it doesn't require an additional function to check divisibility within the range.
+
+The `gcd` function calculates the greatest common divisor, which is used to find the LCM. This version is both more concise and performs better than the original implementation.
+
+*/
