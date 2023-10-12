d20427d0fcc8b2c94c0fb807791696bdc51f1788
Alejandro Leonian <alejandro.leonian@humansecurity.com>
2023-10-12 13:37:36 -0500

    chatgpt's version added

--- Javascript_DSA/Intermediate_Algorithm_Scripting/16-steamRollArray.js
+++ Javascript_DSA/Intermediate_Algorithm_Scripting/16-steamRollArray.js
@@ -18,19 +18,47 @@ function steamrollArray(arr) {
 }
 
 
-// console.log(steamrollArray([1, [2]]));
-
 console.log(steamrollArray([1, {}, [3, [[4]]]]));
 
+/*
 
-// steamrollArray([[["a"]], [["b"]]]) should return ["a", "b"].
+ChatGPT's version:
+function steamrollArray(arr) {
+    const flattenedArray = [];
 
+    function flatten(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                flatten(arr[i]);
+            } else {
+                flattenedArray.push(arr[i]);
+            }
+        }
+    }
 
-[1,
-    [2],
-    [3,
-        [
-            [4]
-        ]
-    ]
-]
+    flatten(arr);
+    return flattenedArray;
+}
+
+// Example usage:
+const nestedArray = [1, [2], [3, [[4]]]];
+const result = steamrollArray(nestedArray);
+console.log(result); // [1, 2, 3, 4]
+
+In this implementation:
+
+We define a flattenedArray to store the flattened elements.
+
+We use a recursive flatten function to traverse the nested arrays. 
+
+If an element is an array, we recursively call flatten on that array. 
+
+If it's not an array, we push it to the flattenedArray.
+
+We call the flatten function on the input array to populate the flattenedArray.
+
+This approach achieves the same result as the original function but doesn't rely on Array.prototype.flat(). 
+
+It provides a custom, recursive solution for flattening nested arrays.
+
+*/
