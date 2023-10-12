cabaa2a3c9e18d52738cd9ba7f99a03f736dc505
Alejandro Leonian <alejandro.leonian@humansecurity.com>
2023-10-12 12:02:05 -0500

    ChatGPT blew my mind for a change

--- Javascript_DSA/Intermediate_Algorithm_Scripting/15-dropElements.js
+++ Javascript_DSA/Intermediate_Algorithm_Scripting/15-dropElements.js
@@ -1,15 +1,43 @@
 function dropElements(arr, func) {
-    for (let i = 0; i < arr.length; i++) {
-        if (func(arr[i])) {
-            break;
+
+    let conditionIsMet = false;
+
+    return arr.filter(item => {
+        if (!conditionIsMet) {
+            if (func(item)) {
+                conditionIsMet = true;
+            }
         }
-        else {
-            arr.shift();
-            i = -1;
-        }
-    }
-    return arr;
+        return conditionIsMet;
+    })
+
 }
 
 console.log(dropElements([1, 2, 3, 4], function (n) { return n >= 3; }));
 
+/*
+
+The mind-blowing version of ChatGPT:
+
+function dropElements(arr, func) {
+    const index = arr.findIndex(func);
+    return index === -1 ? [] : arr.slice(index);
+}
+
+We use arr.findIndex(func) to find the index of the first element that satisfies 
+
+the condition provided by the func function.
+
+If the index is -1, it means no element in the array meets the condition, 
+so we return an empty array [].
+
+If an index is found, we use arr.slice(index) to create a new array 
+starting from that index, effectively dropping the elements that do not meet the condition.
+
+This version of the function achieves the same result as the original code but in a more concise and idiomatic way.
+
+ChatGPT's function does not iterate on each item but finds the first item that satisfies the callback's
+
+need 🤯
+
+*/
