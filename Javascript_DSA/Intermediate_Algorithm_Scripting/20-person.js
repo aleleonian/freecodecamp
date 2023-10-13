0ba772ce73d56a8efe281673de22e01884eb307e
Alejandro Leonian <alejandro.leonian@humansecurity.com>
2023-10-13 11:15:21 -0500

    finished exercise 20'

---
+++ Javascript_DSA/Intermediate_Algorithm_Scripting/20-person.js
@@ -0,0 +1,35 @@
+const Person = function (first, last) {
+
+    let firstName = first;
+    let lastName = last;
+
+    this.getFullName = function () {
+        return firstName + " " + lastName;
+    };
+
+    this.getFirstName = function () {
+        return firstName;
+    };
+
+    this.getLastName = function () {
+        return lastName;
+    };
+
+    this.setFirstName = function (first) {
+        firstName = first;
+    };
+
+    this.setLastName = function (last) {
+        lastName = last;
+    };
+
+    this.setFullName = function (first, last) {
+        firstName = first;
+        lastName = last;
+    };
+
+};
+
+const artist = new Person('Bob', 'Ross');
+
+console.log(artist.setFirstName);
