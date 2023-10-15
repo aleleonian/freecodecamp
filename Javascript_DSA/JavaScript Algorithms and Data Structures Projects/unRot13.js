00cc19dcf3f5eba7ab836b6c46cf36e66a98734c
Alejandro Leonian <alejandro.leonian@humansecurity.com>
2023-10-15 18:12:44 -0500

    chatGPT's version

--- Javascript_DSA/JavaScript Algorithms and Data Structures Projects/unRot13.js
+++ Javascript_DSA/JavaScript Algorithms and Data Structures Projects/unRot13.js
@@ -1,19 +1,62 @@
 
 
 function rot13(str) {
-    let translatedString = "";
     const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    for (let i = 0; i < str.length; i++) {
-        let currentChar = str[i];
-        if (!currentChar.match(/\W+|_|\s+/)) {
-            let charPosition = alphabet.indexOf(currentChar);
-            charPosition = (charPosition + 13) % alphabet.length;
-            currentChar = alphabet[charPosition];
+    return str.split("").map(currentChar => {
+        // is it a non letter character?
+        if (currentChar.match(/\W+|_/)) {
+            return currentChar;
         }
-        translatedString += currentChar;
-    }
-    return translatedString;
+        // if not, let's conver it into the corresponding character
+        else {
+            let charPosition = (alphabet.indexOf(currentChar) + 13) % alphabet.length;
+            return alphabet[charPosition];
+        }
+    })
+        .join("");
+
+    // for (let i = 0; i < str.length; i++) {
+    //     let currentChar = str[i];
+    //     if (!currentChar.match(/\W+|_/)) {
+    //         let charPosition = alphabet.indexOf(currentChar);
+    //         charPosition = (charPosition + 13) % alphabet.length;
+    //         currentChar = alphabet[charPosition];
+    //     }
+    //     translatedString += currentChar;
+    // }
+    // return translatedString;
 }
 
 console.log(rot13("SERR PBQR PNZC"));
+
+/*
+
+ChatGPT's version:
+
+function rot13(str) {
+  return str.replace(/[A-Z]/g, (char) => {
+    const charCode = char.charCodeAt(0);
+    const offset = charCode < 78 ? 13 : -13;
+    return String.fromCharCode(charCode + offset);
+  });
+}
+
+We use the replace function with a regular expression to match uppercase letters (A-Z).
+
+We use the String.fromCharCode and char.charCodeAt functions to calculate the new 
+character based on the ROT13 transformation.
+
+The code inside the arrow function calculates the new character code based on 
+whether it needs to shift forward (e.g., 'A' becomes 'N') or backward (e.g., 'N' becomes 'A') 
+by 13 positions.
+
+This version simplifies the logic and reduces the need for manual character 
+lookups and loops, making the code more concise and easier to understand.
+
+Takeaways:
+
+ChatGPT uses again the replace() method on a string. This is more efficient than turning
+a string into an array and later again into a string.
+
+*/
